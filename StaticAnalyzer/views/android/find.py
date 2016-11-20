4e6af915dba4e7834488d71d3bf452ff6af7a839
Dominik Schlecht <dominik.schlecht@hotmail.de>
2016-11-20 20:40:29 +0100

    Refactored find.py

--- StaticAnalyzer/views/android/find.py
+++ StaticAnalyzer/views/android/find.py
@@ -15,20 +15,38 @@ from MobSF.utils import (
     PrintException
 )
 
+def __for_file(src, dir_name, jfile, request, matches):
+    file_path = os.path.join(src, dir_name, jfile)
+    if "+" in jfile:
+        fp2 = os.path.join(src, dir_name, jfile.replace("+", "x"))
+        shutil.move(file_path, fp2)
+        file_path = fp2
+    fileparam = file_path.replace(src, '')
+    with io.open(
+        file_path,
+        mode='r',
+        encoding="utf8",
+        errors="ignore"
+    ) as file_pointer:
+        dat = file_pointer.read()
+    if request.POST['q'] in dat:
+        matches.append(
+            "<a href='../ViewSource/?file=" + escape(fileparam) +
+            "&md5=" + request.POST['md5'] +
+            "&type=apk'>" + escape(fileparam) + "</a>"
+        )
+    return matches
+
 def run(request):
     """Find in source files."""
     try:
-        match = re.match('^[0-9a-f]{32}$', request.POST['md5'])
-        if match:
-            md5 = request.POST['md5']
-            query = request.POST['q']
-            code = request.POST['code']
+        if re.match('^[0-9a-f]{32}$', request.POST['md5']):
             matches = []
-            if code == 'java':
-                src = os.path.join(settings.UPLD_DIR, md5+'/java_source/')
+            if request.POST['code'] == 'java':
+                src = os.path.join(settings.UPLD_DIR, request.POST['md5']  +'/java_source/')
                 ext = '.java'
-            elif code == 'smali':
-                src = os.path.join(settings.UPLD_DIR, md5+'/smali_source/')
+            elif request.POST['code'] == 'smali':
+                src = os.path.join(settings.UPLD_DIR, request.POST['md5'] + '/smali_source/')
                 ext = '.smali'
             else:
                 return HttpResponseRedirect('/error/')
@@ -37,30 +55,12 @@ def run(request):
             for dir_name, sub_dir, files in os.walk(src):
                 for jfile in files:
                     if jfile.endswith(ext):
-                        file_path = os.path.join(src, dir_name, jfile)
-                        if "+" in jfile:
-                            fp2 = os.path.join(src, dir_name, jfile.replace("+", "x"))
-                            shutil.move(file_path, fp2)
-                            file_path = fp2
-                        fileparam = file_path.replace(src, '')
-                        with io.open(
-                            file_path,
-                            mode='r',
-                            encoding="utf8",
-                            errors="ignore"
-                        ) as file_pointer:
-                            dat = file_pointer.read()
-                        if query in dat:
-                            matches.append(
-                                "<a href='../ViewSource/?file=" + escape(fileparam) +
-                                "&md5=" + md5 +
-                                "&type=apk'>" + escape(fileparam) + "</a>"
-                            )
+                        matches = __for_file(src, dir_name, jfile, request, matches)
         flz = len(matches)
         context = {
             'title': 'Search Results',
             'matches': matches,
-            'term': query,
+            'term': request.POST['q'],
             'found' : str(flz)
         }
         template = "search.html"
