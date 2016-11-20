0c35b2a87814aad1e1de26273c70494733963386
Dominik Schlecht <dominik.schlecht@hotmail.de>
2016-11-20 20:47:50 +0100

    Refactored java.py

--- StaticAnalyzer/views/android/java.py
+++ StaticAnalyzer/views/android/java.py
@@ -14,19 +14,35 @@ from MobSF.utils import (
     PrintException
 )
 
+
+def __for_file(src, dir_name, jfile, request, html):
+    """Add to html for every file."""
+    file_path = os.path.join(src, dir_name, jfile)
+    if "+" in jfile:
+        fp2 = os.path.join(src, dir_name, jfile.replace("+", "x"))
+        shutil.move(file_path, fp2)
+        file_path = fp2
+    fileparam = file_path.replace(src, '')
+    if any(cls in fileparam for cls in settings.SKIP_CLASSES) is False:
+        html += (
+            "<tr><td><a href='../ViewSource/?file=" + escape(fileparam) +
+            "&md5=" + request.GET['md5'] +
+            "&type=" + request.GET['type'] + "'>" +
+            escape(fileparam) + "</a></td></tr>"
+        )
+    return html
+
+
 def run(request):
     """Show the java code."""
     try:
-        match = re.match('^[0-9a-f]{32}$', request.GET['md5'])
-        typ = request.GET['type']
-        if match:
-            md5 = request.GET['md5']
-            if typ == 'eclipse':
-                src = os.path.join(settings.UPLD_DIR, md5 + '/src/')
-            elif typ == 'studio':
-                src = os.path.join(settings.UPLD_DIR, md5 + '/app/src/main/java/')
-            elif typ == 'apk':
-                src = os.path.join(settings.UPLD_DIR, md5 + '/java_source/')
+        if re.match('^[0-9a-f]{32}$', request.GET['md5']):
+            if request.GET['type'] == 'eclipse':
+                src = os.path.join(settings.UPLD_DIR, request.GET['md5'] + '/src/')
+            elif request.GET['type'] == 'studio':
+                src = os.path.join(settings.UPLD_DIR, request.GET['md5'] + '/app/src/main/java/')
+            elif request.GET['type'] == 'apk':
+                src = os.path.join(settings.UPLD_DIR, request.GET['md5'] + '/java_source/')
             else:
                 return HttpResponseRedirect('/error/')
             html = ''
@@ -35,24 +51,12 @@ def run(request):
             for dir_name, sub_dir, files in os.walk(src):
                 for jfile in files:
                     if jfile.endswith(".java"):
-                        file_path = os.path.join(src, dir_name, jfile)
-                        if "+" in jfile:
-                            fp2 = os.path.join(src, dir_name, jfile.replace("+", "x"))
-                            shutil.move(file_path, fp2)
-                            file_path = fp2
-                        fileparam = file_path.replace(src, '')
-                        if any(cls in fileparam for cls in settings.SKIP_CLASSES) is False:
-                            html += (
-                                "<tr><td><a href='../ViewSource/?file=" + escape(fileparam) +
-                                "&md5=" + md5 +
-                                "&type=" + typ + "'>" +
-                                escape(fileparam) + "</a></td></tr>"
-                            )
+                        html += __for_file(src, dir_name, jfile, request, html)
         context = {
             'title': 'Java Source',
             'files': html,
-            'md5': md5,
-            'type': typ,
+            'md5': request.GET['md5'],
+            'type': request.GET['type'],
         }
 
         template = "java.html"
